81b985e8d723e2e3ef7486bdda6443bfc979b606
Damon Rocha <dmarcr1997@gmail.com>
2022-08-15 17:12:02 -0400

    Added fullscreen button to other components and moved bg image

--- mossy-nodes/src/app/contact/contact.component.ts
+++ mossy-nodes/src/app/contact/contact.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
   @Output() onCloseTab: EventEmitter<any> = new EventEmitter();
+  fullScreen: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
@@ -14,6 +15,14 @@ export class ContactComponent implements OnInit {
 
   closeButtonClicked(): void {
     this.onCloseTab.emit([]);
+  }
+
+  getStyle(): string {
+    return this.fullScreen ?  "card-full-screen" : "card-regular";
+  }
+
+  fullScreenButtonClicked(): void { 
+    this.fullScreen = !this.fullScreen;
   }
 
 }
--- mossy-nodes/src/app/resume/resume.component.ts
+++ mossy-nodes/src/app/resume/resume.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class ResumeComponent implements OnInit {
   @Output() onCloseTab: EventEmitter<any> = new EventEmitter();
+  fullScreen: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
@@ -14,6 +15,14 @@ export class ResumeComponent implements OnInit {
 
   closeButtonClicked(): void {
     this.onCloseTab.emit([]);
+  }
+
+  getStyle(): string {
+    return this.fullScreen ?  "card-full-screen" : "card-regular";
+  }
+
+  fullScreenButtonClicked(): void { 
+    this.fullScreen = !this.fullScreen;
   }
 
 }
