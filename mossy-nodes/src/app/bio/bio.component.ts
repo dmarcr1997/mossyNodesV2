7d219cfb94a46600678e0c00df0446d6bdf52f83
Damon Rocha <dmarcr1997@gmail.com>
2022-08-15 17:05:40 -0400

    Added fullscreen button to bio component

--- mossy-nodes/src/app/bio/bio.component.ts
+++ mossy-nodes/src/app/bio/bio.component.ts
@@ -7,7 +7,7 @@ import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 })
 export class BioComponent implements OnInit {
   @Output() onCloseTab: EventEmitter<any> = new EventEmitter();
-
+  fullScreen: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
@@ -15,6 +15,14 @@ export class BioComponent implements OnInit {
 
   closeButtonClicked(): void {
     this.onCloseTab.emit([]);
+  }
+
+  getStyle(): string {
+    return this.fullScreen ?  "card-full-screen" : "card-regular";
+  }
+
+  fullScreenButtonClicked(): void { 
+    this.fullScreen = !this.fullScreen;
   }
 
 }
