b7b54c041dfaabe8d418a693bb80c1c186341351
Damon Rocha <dmarcr1997@gmail.com>
2022-08-14 22:42:26 -0400

    Added other components

--- mossy-nodes/src/app/app.module.ts
+++ mossy-nodes/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
 import { IntroComponent } from './intro/intro.component';
 import { BioComponent } from './bio/bio.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     IntroComponent,
-    BioComponent
+    BioComponent,
+    PortfolioComponent,
+    ResumeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
